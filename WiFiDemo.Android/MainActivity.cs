899f38cb73953c8753d3758f4ee997abbfc3e546
BridgeLabz <bridgelabz@BridgeLabzs-MacBook-Pro.local>
2019-08-30 19:31:12 +0530

    first commit

---
+++ WiFiDemo.Android/MainActivity.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Net.Wifi;
+using Android.Content;
+using System.Linq;
+
+namespace WiFiDemo.Droid
+{
+    [Activity(Label = "WiFiDemo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+            SetContentView(Resource.Layout.MainLayout);
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        //public bool JoinWifi(string ssid, string key)
+        //{
+        //    try
+        //    {
+        //        WifiConfiguration wifiConfig = new WifiConfiguration();
+        //        wifiConfig.Ssid = String.Format("\"{0}\"", ssid);
+        //        wifiConfig.PreSharedKey = String.Format("\"{0}\"", key);
+        //        WifiManager wifiManager = (WifiManager)GetSystemService(Context.WifiService);
+        //        wifiManager.SetWifiEnabled(true);
+        //        // Get or add network to known list
+        //        int netId;
+        //        var network = wifiManager.ConfiguredNetworks.FirstOrDefault(cn => cn.Ssid == ssid);
+        //        if (network != null) netId = network.NetworkId;
+        //        else
+        //        {
+        //            netId = wifiManager.AddNetwork(wifiConfig);
+        //            wifiManager.SaveConfiguration();
+        //        }
+        //        // Make sure network hasn't changed
+        //        wifiManager.UpdateNetwork(wifiConfig);
+        //        var currConn = wifiManager.ConnectionInfo;
+        //        if (currConn != null && currConn.NetworkId != netId)
+        //        {
+        //            wifiManager.Disconnect();
+        //            wifiManager.EnableNetwork(netId, true);
+        //            wifiManager.Reconnect();
+        //        }
+        //        // Make sure system doesn't save power by disabling WiFi
+        //        wifiManager.CreateWifiLock(Android.Net.WifiMode.Full, "myLockId"); return true;
+        //    }
+        //    catch (Exception x)
+        //    {
+        //        return false;
+        //    }
+        //}
+    }
+}
---
+++ WiFiDemo/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Plugin.Connectivity;
+using Plugin.Connectivity.Abstractions;
+
+namespace WiFiDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            MainPage = CrossConnectivity.Current.IsConnected ? (Page)new MainPage() : new Page1();
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            CrossConnectivity.Current.ConnectivityChanged += HandleConnectivityChanged;
+        }
+
+        private void HandleConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+        {
+            Type Currentpage = this.MainPage.GetType();
+            if(e.IsConnected && Currentpage !=  typeof(MainPage))
+            {
+                this.MainPage = new MainPage();
+
+            }
+            else if (!e.IsConnected && Currentpage != typeof(Page1))
+                this.MainPage = new Page1();
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
